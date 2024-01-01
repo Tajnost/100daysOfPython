c806adacedd8c2a0f1ad47038930acd7e7749fc0
Tajnost <155396761+Tajnost@users.noreply.github.com>
2024-01-01 23:51:01 +0100

    Delete 1. Day 1 directory

--- 1. Day 1/Project1-Band-Name-Generator.py
+++
@@ -1,8 +0,0 @@
-###
-### Day 1 - 100 days of Python - Project 1
-###
-
-print("Welcome to the Band Name Generator!")
-city = input("What is the name of the city you grew up in?\n")
-pet = input("What is you pet's name?\n")
-print("You band name could be " + city + " " + pet)
