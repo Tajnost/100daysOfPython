918d0c93dca0a30ac85f8bc22da218855522b4a8
Tajnost <155396761+Tajnost@users.noreply.github.com>
2024-01-13 23:54:58 +0100

    Add files via upload

---
+++ Day 14/ObjectOrientedPrograming.py
@@ -0,0 +1,29 @@
+# from turtle import Turtle,Screen
+# from time import sleep
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.position()
+
+# for i in range(100):
+#     timmy.forward(i)
+#     timmy.left(i)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# x = PrettyTable()
+
+# x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+# x.add_row(["Adelaide", 1295, 1158259, 600.5])
+# x.add_row(["Brisbane", 5905, 1857594, 1146.4])
+# x.add_row(["Darwin", 112, 120900, 1714.7])
+# x.add_row(["Hobart", 1357, 205556, 619.5])
+# x.add_row(["Sydney", 2058, 4336374, 1214.8])
+# x.align = "l" ## change style to go left alight
+
+# print(x)
